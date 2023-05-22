b5e801df31d827e05830fe27479e1c59bee24178
10994015 <a0938599191@gmail.com>
2023-05-22 21:22:01 +0800

    修正購物車

--- web/data.php
+++ web/data.php
@@ -18,7 +18,8 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/style.css">
     <title>線上二手書局</title>
     <link rel="stylesheet" href="../css/search.css">
@@ -28,31 +29,58 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <!-- GET POST -->
  
     <?php include('./header.php') ?>
+    <div class="cart" id="cartui">
+        <i class="fa-solid fa-circle-xmark" id="cartClose"></i>
+        <h2>YOUR CART</h2>
+        <div class="cart-content">
+
+        </div>
+        <div class="total">
+            <div class="total-title">Total</div>
+            <div class="total-price">$0</div>
+        </div>
+        <button type="button" class="buy-btn">立刻下單</button>
+        <ion-icon name="close" id="close-cart"></ion-icon>
+    </div>
     <div class="shopping">
         <div class="search-bar">
             <form action="./search.php" method="GET"> <!-- 這裡跑版了要改 -->
                 <input type="text" name="s" placeholder="搜尋...">
-                <button type="submit"><ion-icon name="search-outline"></ion-icon></button>
+                <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
             </form>
-            <div class="cart-icon">
-                <a href="#">
-                    <ion-icon name="cart-outline" id="cart-btn"></ion-icon>
+            <div class="cart-icon" >
+                <a href="javascript:;" id="cartBtn">
+                    <!-- <ion-icon name="cart-outline" id="cart-btn"></ion-icon> -->
+                    <i class="fa-solid fa-cart-shopping"></i>
                 </a>
-                <div class="cart">
-                    <h2>YOUR CART</h2>
-                    <div class="cart-content">
-
-                    </div>
-                    <div class="total">
-                        <div class="total-title">Total</div>
-                        <div class="total-price">$0</div>
-                    </div>
-                    <button type="button" class="buy-btn">立刻下單</button>
-                    <ion-icon name="close" id="close-cart"></ion-icon>
-                </div>
             </div>
         </div>
             
+        <script>
+            const cartBtn = document.getElementById('cartBtn')
+            const cartui = document.getElementById('cartui')
+            const cartClose =document.getElementById('cartClose')
+            let open = false
+            console.log(123);
+            cartClose.addEventListener('click', ()=>{
+                open = false
+                if(open){
+                    cartui.style.right = '0'
+                }else{
+                    cartui.style.right = '-100%'
+                }
+            })
+            cartBtn.addEventListener('click' , ()=>{
+                open = true
+                if(open){
+                    cartui.style.right = '0'
+                }else{
+                    cartui.style.right = '-100%'
+                }
+            })
+        </script>
+
+
         <div class="container">
             <?php if (count($products) == 0){?>
                 <h2 class="title">查無此商品。</h2>
@@ -97,9 +125,13 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         </div>
     </div>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src="main.js"></script>
+   
+    <!-- <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> -->
+    <!-- <script src="../js/main.js"></script> -->
+
+
+   
 </body>
 
 </html>
