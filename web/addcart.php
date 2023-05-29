ce2506e001881502fc1a8d030b1da4608d507a54
pat0807 <s363157@gmail.com>
2023-05-29 21:36:40 +0800

    0529 0936

--- web/addcart.php
+++ web/addcart.php
@@ -2,12 +2,28 @@
 
 $id =  $_POST['id'];
 $quantity =  $_POST['quantity'];
-$_SESSION['number'] = 123;
 
-$_COOKIE['cart_item'] = [
-    'id'=>$id, 'quantity'=>$quantity
-];
+$cart_items = [];
+array_push($cart_items,['id'=>$id,'quantity'=>$quantity]);
 
-print_r($_COOKIE['cart_item']);
+print_r($cart_items);
+
+json_encode($cart_items);
+
+echo json_encode($cart_items);
+setcookie('cart_items',json_encode($cart_items),time() + 3600,'/');
+
+
+// $_COOKIE['cart_items']=[
+//     [
+//         'id'=>1,
+//         'quantity'=>8
+//     ],
+//     [
+//         'id'=>4,
+//         'quantity'=>3
+//     ]
+// ];
+
 
 ?>
