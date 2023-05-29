3c860d3a2157d5df6dbbbfc4d740215c6ad69239
10994015 <a0938599191@gmail.com>
2023-05-29 21:46:18 +0800

    create shop css

--- web/cart.php
+++ web/cart.php
@@ -2,7 +2,50 @@
     <i class="fa-solid fa-circle-xmark" id="cartClose"></i>
     <h2>YOUR CART</h2>
     <div class="cart-content">
-
+        <div class="cart-item">
+            <img src="" alt="" />
+            <div class="text">
+                <h3>Product name</h3>
+                <p>價錢:$1000</p>
+                <label for="">
+                    數量:
+                    <input type="number"  value="1" />
+                </label>
+            </div>
+        </div>
+        <div class="cart-item">
+            <img src="" alt="" />
+            <div class="text">
+                <h3>Product name</h3>
+                <p>價錢:$1000</p>
+                <label for="">
+                    數量:
+                    <input type="number"  value="1" />
+                </label>
+            </div>
+        </div>
+        <div class="cart-item">
+            <img src="" alt="" />
+            <div class="text">
+                <h3>Product name</h3>
+                <p>價錢:$1000</p>
+                <label for="">
+                    數量:
+                    <input type="number"  value="1" />
+                </label>
+            </div>
+        </div>
+        <div class="cart-item">
+            <img src="" alt="" />
+            <div class="text">
+                <h3>Product name</h3>
+                <p>價錢:$1000</p>
+                <label for="">
+                    數量:
+                    <input type="number"  value="1" />
+                </label>
+            </div>
+        </div>
     </div>
     <div class="total">
         <div class="total-title">Total</div>
