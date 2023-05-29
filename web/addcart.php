7db4fcda977a5cb59f1dc431ce83f5da56641408
10994015 <a0938599191@gmail.com>
2023-05-29 22:42:00 +0800

    0529

--- web/addcart.php
+++ web/addcart.php
@@ -10,11 +10,11 @@ $isset = false;
 if(isset($_COOKIE['cart_items'])){
     $cart_items = json_decode($_COOKIE['cart_items'],true);
     print_r($cart_items);
-    foreach($cart_items as $cart_item){
+    foreach($cart_items as &$cart_item){
         if ($cart_item['id'] == $id){
             $isset = true; 
-            echo 123456789;
             $cart_item['quantity'] = intval($cart_item['quantity']) + intval($quantity);
+       
         }
     }
     if ($isset == false){
@@ -29,18 +29,6 @@ if(isset($_COOKIE['cart_items'])){
 print_r($cart_items);
 
 setcookie('cart_items',json_encode($cart_items),time() + 3600,'/');
-
-
-// $_COOKIE['cart_items']=[
-//     [
-//         'id'=>1,
-//         'quantity'=>8
-//     ],
-//     [
-//         'id'=>4,
-//         'quantity'=>3
-//     ]
-// ];
 
 
 ?>
